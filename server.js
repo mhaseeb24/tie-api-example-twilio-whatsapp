71500d0679fc3d9df74ae7e4535e053ebf4fb232
Mohammad Haseeb <76884207+mhaseeb24@users.noreply.github.com>
2022-01-16 21:55:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+require("dotenv").config();
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
